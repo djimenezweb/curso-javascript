698083345305dfd725d118ff7cdfb6716516ec18
Daniel Jiménez <117752348+djimenezweb@users.noreply.github.com>
2023-01-10 23:40:28 +0100

    Ejercicio DOM terminado

--- clase5/js/dom.js
+++ clase5/js/dom.js
@@ -10,12 +10,18 @@
 // - Partiendo de este HTML, crea una función que reciba UN SOLO ELEMENTO (Elige uno de los 3) y muestre toda la información propia y de los otros dos elementos, padres, hermanos o hijos en función del elemento que hayas elegido. La función debe mostrar por consola el id, las clases, el textContent, el ancho y el alto de los tres elementos con un mensaje que no parezca de un robot 😊.
 
 const dom = element => {
-  console.log(`Soy ${element.id}. Tengo las clases ${element.classList}. Mi texto es ${element.textContent}. Mido ${element.clientWidth}px de ancho y ${element.clientHeight}px de alto.`)
+  console.log(`Soy ${element.id}. Tengo las clases ${element.classList}. Mi texto es "${element.textContent}". Mido ${element.clientWidth}px de ancho y ${element.clientHeight}px de alto.`)
+  if (element.children.length > 0) {
   console.log(`Tengo ${element.childElementCount} hijos: ${element.children[0].id} y ${element.children[1].id}`)
-  console.log(`Mi primer hijo es ${element.children[0].id.toUpperCase()}. Tiene las clases ${element.children[0].classList}. Su texto es "${element.children[0].textContent}". Mide ${element.children[0].clientWidth}px de ancho y ${element.children[0].clientHeight}px de alto.`)
-  console.log(`Mi segundo hijo es ${element.children[1].id.toUpperCase()}. Tiene las clases ${element.children[1].classList}. Su texto es "${element.children[1].textContent}". Mide ${element.children[1].clientWidth}px de ancho y ${element.children[1].clientHeight}px de alto.`)
+  console.log(`Mi primer hijo es ${element.children[0].id}. Tiene las clases ${element.children[0].classList}. Su texto es "${element.children[0].textContent}". Mide ${element.children[0].clientWidth}px de ancho y ${element.children[0].clientHeight}px de alto.`)
+  console.log(`Mi segundo hijo es ${element.children[1].id}. Tiene las clases ${element.children[1].classList}. Su texto es "${element.children[1].textContent}". Mide ${element.children[1].clientWidth}px de ancho y ${element.children[1].clientHeight}px de alto.`)
+} else {
+  console.log(`Mi padre es ${element.parentElement.id}. Tiene las clases ${element.parentElement.classList}. Su texto es "${element.parentElement.textContent}". Mide ${element.parentElement.clientWidth}px de ancho y ${element.parentElement.clientHeight}px de alto.`)
+  
+  if (element.nextElementSibling !== null) {console.log(`Mi hermano menor es ${element.nextElementSibling.id}. Tiene las clases ${element.nextElementSibling.classList}. Su texto es "${element.nextElementSibling.textContent}". Mide ${element.nextElementSibling.clientWidth}px de ancho y ${element.nextElementSibling.clientHeight}px de alto.`)} else if (element.previousSibling !== null) {console.log(`Mi hermano mayor es ${element.previousElementSibling.id}. Tiene las clases ${element.previousElementSibling.classList}. Su texto es "${element.previousElementSibling.textContent}". Mide ${element.previousElementSibling.clientWidth}px de ancho y ${element.previousElementSibling.clientHeight}px de alto.`)} else {return}
+}
 };
 
 dom(document.getElementById('header'));
 
-//console.dir(document.getElementById('header'))
+//console.dir(document.getElementById('main-title'))
