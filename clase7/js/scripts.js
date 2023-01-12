bd77249d80f65b5f32da76c5f9e0926547612b91
Daniel Jiménez <djimenezweb@gmail.com>
2023-01-12 18:54:47 +0100

    Scroll bar

---
+++ clase7/js/scripts.js
@@ -0,0 +1,22 @@
+// # Ejercicios
+
+// ## Objeto Window
+
+// - Crea una web que tenga un alto mínimo de 1000vh y muestra en un h1 "Te has desplazado X píxeles" cada vez que hagas scroll. Hazlo con un función llamada handleScroll.
+const pixelsElement = document.getElementById('pixels');
+const scrollBarElement = document.getElementById('scroll-bar');
+
+const handleScroll = ev => {
+  pixelsElement.textContent = ev.target.scrollingElement.scrollTop;
+  scrollBarElement.style.width =
+    (ev.target.scrollingElement.scrollTop * 100) /
+      (ev.target.scrollingElement.scrollHeight - window.innerHeight) +
+    '%';
+};
+
+window.addEventListener('scroll', handleScroll);
+
+// En este caso, ev.target equivale a window porque window ha sido quien ha desencadenado el evento (mediante un scroll)
+// Si fuese un clic, ev.target equivale al sitio donde se ha hecho clic.
+
+// - En esa misma web, crea un elemento que se coloque SIEMPRE en la parte superior y que va a representar la cantidad de scroll que hemos hecho, cuando estemos arriba del todo tendrá un "ancho" de 0 y cuando estemos abajo del todo tendrá un "ancho" que ocupe toda la pantalla.
